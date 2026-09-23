835b9032595851cb40d3b5ed864390508b69ecf8
d6064791-dotcom <d6064791@gmail.com>
2026-09-23 14:00:24 -0400

    Update practice_03_conditionals copy.js

--- practice_03_conditionals copy.js
+++ practice_03_conditionals copy.js
@@ -98,7 +98,7 @@ console.log(fizzBuzz(3)); // "Fizz"
 console.log(fizzBuzz(5)); // "Buzz"
 console.log(fizzBuzz(15)); // "FizzBuzz"
 console.log(fizzBuzz(7)); // "7"
-*/
+
 // ---------- Problem 4: Shipping Cost Calculator ----------
 // If isMember is true:
 //   weight <= 5  -> 0 (free)
@@ -108,7 +108,23 @@ console.log(fizzBuzz(7)); // "7"
 //   weight <= 5  -> 8
 //   weight > 5   -> 12
 function getShippingCost(weight, isMember) {
-  // TODO: your code here (nested conditional — check isMember first, then weight)
+  if (isMember === true && weight <= 5) {
+    return "0"
+  }
+  else if (isMember === true && weight > 5) {
+    return "3"
+  } 
+  else if (isMember === false && weight <= 1) {
+    return "5"
+  } 
+  else if (isMember === false && weight <= 5) {
+    return "8"
+  } 
+  else if (isMember === false && weight > 5) {
+    return "12"
+  } 
+  
+  //weight and membership, check membership first then and. then elif no mem 
 }
 
 console.log(getShippingCost(3, true)); // 0
@@ -116,14 +132,13 @@ console.log(getShippingCost(8, true)); // 3
 console.log(getShippingCost(0.5, false)); // 5
 console.log(getShippingCost(4, false)); // 8
 console.log(getShippingCost(10, false)); // 12
-/*
+*/
 // ---------- Stretch (optional) ----------
 // Rewrite the even/odd check from Problem 1 using the ternary operator
 // instead of if / else.
 function isEvenTernary(num) {
-  // TODO: your code here
+  return num % 2 === 0 ? "true" : 'false' ;
 }
 
 console.log(isEvenTernary(4)); // true
 console.log(isEvenTernary(7)); // false
-*/
